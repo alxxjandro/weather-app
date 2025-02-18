7f9bfb334be60c957f70e696c89c0f211fa6eaa3
Alonso Alarcon <155941412+alxxjandro@users.noreply.github.com>
2025-02-17 19:02:31 -0600

    Creating two functions that use de unsplash API to get images as the bg

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import "./general.css";
+
+async function searchImage(search, index){
+    const response = await fetch(`https://api.unsplash.com/search/photos?query=${search}&client_id=LTwuJxAgrhNFWOpd9BBr7EWyr6YTPtUdvnsBPr-pI3Y`, {mode: 'cors'});
+    const images = await response.json();
+    console.log(images.results[index])
+    return images.results[index].urls.regular;
+}
+
+async function changeBackground(background, search , index){
+    const image = await searchImage(search, index);
+    background.src = image;
+}
+
+
+const image = document.querySelector("#imagen");
+changeBackground(image, "f1", 7)
